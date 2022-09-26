8e0b9082395dc4f342cfc00b1d375a2461058208
Latha <lathananjappa25@gmail.com>
2022-09-26 10:11:33 +0530

    adding array

---
+++ SuperMarket.java
@@ -0,0 +1,33 @@
+class SuperMarket
+{
+static String superMarketName="MetroWholesale";
+static String vegetables[]={"Onion","Carrot","Ginger","Tomato","Drum Stick","Soorekai","Cucumber","Pumpkin"};
+static String perfume[]={"fogg","Axe","Magnet","Parker"};
+static String cloths[]={"T Shirt","Jeans","Shirt","Cotton Pant"};
+static String fruits[]= {"Apple","Orange","kiwi","Banana","Pomogranate","Grapes","Papaya"};
+static String electronicDevices={"Mobile","Laptop","earPhone","Watch","Television"ng,"Airconditioner"};
+static String grocery[]={"Bread","Salt","Sugar","Eggs","Cheese","Milk"};
+static String cosmetics[]={"Moisturizers","perfume","lipsticks","nail polish","hair colours"};
+static String meat[]={"chicken","Lamb","Goat","beaf","Turkey","Duck"};
+static String choclates[]={"Dairy Milk","Milky Bar", "Snickers","lollypop","5Star","phuse"};
+static String energyDrink[]={"Redbull","ORS","ORSL","Sting","Monster"};
+static String shoes[]={"Puma","Adidas","Nike","Reebok","US Polo"};
+static String kitchenUtensils[]={"mugs","buckets","knife","spoon","glass"};
+public static void main(String a[]){
+System.out.println("Welcome to " + superMarketName);
+System.out.println("List of vegetable");
+System.out.println(vegetables[0] +" "+
+vegetables[1]+" "+
+vegetables[2] +" "+
+vegetables[3] +" "+
+vegetables[4] +" "+
+vegetables[5] +" "+
+vegetables[6]+" "+
+vegetables[7]);
+System.out.println("List of perfume");
+System.out.println(perfume[0] +" "+
+perfume[1]+" "+
+perfume[2]+" "+
+perfume[3]);
+}
+}
